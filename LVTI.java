c650372149bc213c74758683798aa179681b54b6
sahilpatil9008@gmail.com <work.sahil.21211@gmail.com>
2026-02-12 17:36:46 +0530

    Local Variable Type Interface in Java Concept

---
+++ LVTI.java
@@ -0,0 +1,29 @@
+public class LVTI {
+
+    public static void main(String[] args) {
+
+    int b=90;
+    var m=90;
+    int n;
+    var k="Sahil";
+    
+
+    var nums=new int[3];
+    nums[0]=78;
+    nums[1]=34;
+    nums[2]=45;
+    System.out.println(m);
+    System.out.println(k);
+    for(int d:nums)
+    {
+
+        System.out.print(d+",");
+    }
+    System.out.println();
+
+    var onk=new LVTI();
+        
+    }
+
+    
+}
